e62bb372f431c877ca1c01f3bc3bbb117ce3d4d8
Luca Ferroni <luca@befair.it>
2021-10-08 23:41:32 +0200

    Lasciamo perdere le liste e affidiamoci alla memoria

--- easy_life_by_checking.py
+++ easy_life_by_checking.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 
+# Some stuff in your memory
+MEMORY = [ "fishing", "solve problem 1", "running", "make a call"]
+
 # Durante il giorno
 while True:
 
-    # Scegli casualmente un elemento dalla tua lista di cose da fare
-    what_i_like_now = random.choice(... set of items in my todo list…)
+    # Scegli casualmente un elemento da ciò che hai in mente ora
+    what_i_like_now = random.choice(MEMORY)
 
     # Imposta una variabile di nome "end", avremo potuto chiamarla "finito" a False
     end = False
@@ -16,7 +19,7 @@ while True:
         # Fai uno step dell'elemento scelto
         end = do_a_step(what_i_like_now)
 
-        # Se "end" (o "finito") è vera, oppure se avviene un evento
+        # Se "end" (o "finito") è vera, oppure se avviene un evento che interrompe
         if end or there_is(an_interrupt):
 
             # esci da questo ciclo
